45e97eaa80c9ef3519c56002f2180ec47704e2d0
Abdullah-Ahmedd <163368044+Abdullah-Ahmedd@users.noreply.github.com>
2025-07-08 16:46:09 +0300

    Update Assigment1.v

--- Temsah/Assigments/Assigment1.v
+++ Temsah/Assigments/Assigment1.v
@@ -16,32 +16,42 @@ output reg OUT3
 reg D1,D2,D3;
 reg nor1,or1;
 
+
+  
 always@(posedge CLK)
 begin
 OUT1<=D1;
 end
+  
 always@(posedge CLK)
 begin
 OUT2<=D2;
 end
+  
 always@(posedge CLK)
 begin
 OUT3<=D3;
 end
 
+
+  
 always@(IN1 , IN2 , IN3)
 begin
 nor1= ~(IN1 | IN2);
 D1= ~(nor1 & IN3 );
 end
+  
 always@(IN2 , IN3)
 begin
 D2=~(IN2 & IN3);
 end
+  
 always@(IN2 , IN3)
 begin
 or1=(IN3 | ~IN4);
 D3=(or1 | IN5);
 end
 
+
+  
 endmodule
