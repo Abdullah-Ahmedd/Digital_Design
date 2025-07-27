7baa2d1be3a7ca752c5bc30113d6e35459c17aa2
Abdullah_Ahmedd <abdullahahmedcm19@gmail.com>
2025-07-27 21:49:27 +0300

    updated comments for my mistakes

--- Temsah/Assigments/UP_DOWN_Counter/Up_Dn_Counter.v
+++ Temsah/Assigments/UP_DOWN_Counter/Up_Dn_Counter.v
@@ -1,3 +1,12 @@
+/*
+What you did wrong in this RTL design
+
+here if down=1 and low=1 and up=1 and high =0 (which is logical as low=1)
+so the first else if will not be excuted howerver the second else if will be 
+excuted making the counter increment while down=1 which violates the degisn 
+so the second else if should be else if (up && !high && !down) 
+
+*/
 module Up_Dn_Counter
 (
 
@@ -23,15 +32,15 @@ always@(posedge CLK)
         Counter<=IN;
         else if( Down==1'b1 && !Low) 
         Counter<=Counter-1;
-        else if( Up==1'b1 && !High )
-        Counter<=Counter+1;
+        else if( Up==1'b1 && !High ) //it should be else if (up && !high && !down) 
+        Counter<=Counter+1;                             
 
     end
 
 always@(*)
     begin
         
-        if(Counter==5'd31) // Set High flag when Counter reaches 31
+        if(Counter==5'd31) // Set High flag when Counter reaches 31 
         High=1'b1;
         else High=1'b0;
 
